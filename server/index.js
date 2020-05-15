c58500507f898e698b234d78033f9399788d2add
Johnson Zhong <zhsh@umich.edu>
2020-05-15 19:18:12 -0400

    Use config in code directly

--- server/index.js
+++ server/index.js
@@ -3,18 +3,6 @@
  */
 const {startWebSocketServer} = require("./websocket_server");
 const startHTTPServer = require("./http_server");
-const fs = require("fs");
 
-// read configuration
-fs.readFile("../game.json", "utf-8", (err, data)=> {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    const config = JSON.parse(data);
-
-    // start various servers
-    console.log("game configuration retrieved");
-    const server = startHTTPServer(process.env.PORT || config.port);
-    startWebSocketServer(config.wsPath, server);
-});
+const server = startHTTPServer(process.env.PORT || 80);
+startWebSocketServer('/ws', server);
